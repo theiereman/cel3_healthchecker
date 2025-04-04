cdf5f7ead8989a526fa7b6f8900b6c5d69f92291
theiereman <thomas.eyermann@protonmail.com>
2025-04-04 16:04:37 +0200

    chore: rubocop + cleanup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-[{ name: "Demo", url: "https://demo.courtier-en-ligne.com/health_check.json" },
+[ { name: "Demo", url: "https://demo.courtier-en-ligne.com/health_check.json" },
  { name: "DRA", url: "https://dra.courtier-en-ligne.com/health_check.json" },
  { name: "CVA", url: "https://cva.courtier-en-ligne.com/health_check.json" },
  { name: "Angelus", url: "https://angelus.courtier-en-ligne.com/health_check.json" },
